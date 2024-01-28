5078a7c2e88686597045f70fde540ad9f40ed59b
Grosu Denis <denisgrosu790@gmail.com>
2024-01-28 17:43:51 +0200

    action with animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,28 +2,51 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]private float playerHealth = 100f;
     private float horizontal;
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
-
+	private bool attacking = false;
+	[SerializeField] private float playerHealth = 100f;
+	[SerializeField] private float timetoAtack = 0.25f;
+	[SerializeField] private float timer = 0f;
+	[SerializeField] private GameObject attackArea =default;
+    [SerializeField] private Animator animator;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
+	void Start()
+	{
+		attackArea = transform.GetChild(0).gameObject;
+	}
     private void Update()
     {
+		if(Input.GetKeyDown(KeyCode.E))
+		{	
+			animator.SetBool("isAtacking",true);
+			Debug.LogError("Attack");
+		}
+		else
+		{	
+			animator.SetBool("isAtacking",false);
+		}
         horizontal = Input.GetAxisRaw("Horizontal");
         Flip();
-
+		animator.SetFloat("speed", Mathf.Abs(horizontal));
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
+			animator.SetBool("isJumping",true);
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
+		else
+		{
+			animator.SetBool("isJumping",false);
+		}
 
         if (Input.GetButtonDown("Jump") && rb.velocity.y > 0f)
-        {
+        {	
+			
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
         
